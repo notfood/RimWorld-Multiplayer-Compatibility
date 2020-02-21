8708ae5e0e11bae4481d452edf1dc89a7bf1664b
Cody Spring <codyspring@rocketmail.com>
2020-02-21 17:03:34 -0400

    Mortar Accuracy: Support Added
    Added support for Mortar Accuracy. One Rand call needed patching. Tested by having 10 colonists fire mortars all day. No desyncs.

---
+++ Source/Mods/MortarAccuracy.cs
@@ -0,0 +1,33 @@
+﻿using Harmony;
+using Multiplayer.API;
+using Verse;
+
+namespace Multiplayer.Compat
+{
+    /// <summary>Mortar Accuracy by Hob Took</summary>
+    /// <see href="https://steamcommunity.com/sharedfiles/filedetails/?id=1729446857"/>
+    /// contribution to Multiplayer Compatibility by Cody Spring
+    [MpCompatFor("Mortar Accuracy")]
+    class MortarAccuracyCompat
+    {
+        public MortarAccuracyCompat(ModContentPack mod)
+        {
+            //RNG Fix
+            {
+                var methods = new[] {
+                    AccessTools.Method("MortarAccuracy.Verb_LaunchProjectile_MortarMod:FireProjectile"),
+                };
+
+                foreach (var method in methods)
+                {
+                    MpCompat.harmony.Patch(method,
+                        prefix: new HarmonyMethod(typeof(MortarAccuracyCompat), nameof(FixRNGPre)),
+                        postfix: new HarmonyMethod(typeof(MortarAccuracyCompat), nameof(FixRNGPos))
+                    );
+                }
+            }
+        }
+        static void FixRNGPre() => Rand.PushState();
+        static void FixRNGPos() => Rand.PopState();
+    }
+}
