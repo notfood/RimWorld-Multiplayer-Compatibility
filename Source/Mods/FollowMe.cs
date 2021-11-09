486ac124df1b0509bde6c4b63c7cce353a14c31c
Papy Chacal <emilien.bauer@etu.unistra.fr>
2021-11-09 15:52:28 +0100

    FollowMe: Seed the Rand call to avoid desync. Seems to work now.

---
+++ Source/Mods/FollowMe.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using Multiplayer.API;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Verse;
+
+namespace Multiplayer.Compat
+{
+    /// <summary>Misc. Robots by HaploX1</summary>
+    /// <see href="https://steamcommunity.com/sharedfiles/filedetails/?id=724602224"/>
+    [MpCompatFor("Fluffy.FollowMe")]
+    class FollowMe
+    {
+        public FollowMe(ModContentPack mod) => LongEventHandler.ExecuteWhenFinished(LatePatch);
+        private static void LatePatch()
+        {
+            PatchingUtilities.SeedVerseRand("FollowMe.CinematicCamera:FollowNewSubject");
+        }
+    }
+}
