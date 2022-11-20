25603f2c5c3809d62e4fecbb2ac146d131b60a9c
Yolopanther12 <Yolopanther12@users.noreply.github.com>
2022-11-20 01:32:15 -0800

    Mountain Miner (Continued): Support Added
    Patched Unity RNG in Drill method.

---
+++ Source/Mods/MountainMinerContinued.cs
@@ -0,0 +1,15 @@
+﻿using Verse;
+
+namespace Multiplayer.Compat
+{
+    /// <summary>Mountain Miner (Continued) by Mlie</summary>
+    /// <see href="https://steamcommunity.com/sharedfiles/filedetails/?id=2135339386"/>
+    [MpCompatFor("mlie.mountainminer")]
+    class MountainMinerContinued
+    {
+        public MountainMinerContinued(ModContentPack mod)
+        {
+            PatchingUtilities.PatchUnityRand("MountainMiner.Building_MountainDrill:Drill", false);
+        }
+    }
+}
